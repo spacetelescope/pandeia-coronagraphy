f250a4fab5a281d025e8978101eaba02da306907
Brian York <polemarkh@gmail.com>
2018-11-21 15:34:18 -0500

    Trying a change to where the new PSF library is added to things.

--- jwst_pancake/pandeia_subclasses.py
+++ jwst_pancake/pandeia_subclasses.py
@@ -375,6 +375,7 @@ class CoronagraphyConvolvedSceneCube(pandeia.engine.astro_spectrum.ConvolvedScen
         self._log("debug", "CORONAGRAPHY SCENE CUBE ACTIVATE!")
         pandeia.engine.astro_spectrum.SPECTRAL_MAX_SAMPLES = self._max_samples
         super(CoronagraphyConvolvedSceneCube, self).__init__(scene, instrument, background, psf_library, webapp)
+        self.psf_library = CoronagraphyPSFLibrary()
 
     @property
     def _max_samples(self):
