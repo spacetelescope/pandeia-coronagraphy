b8176c58b25ac7537df663154284ca7c87054419
brynickson <bnickson@stsci.edu>
2019-12-05 00:45:59 -0500

    Changes to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,7 +65,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'stsci_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 html_theme_path = [stsci_rtd_theme.get_html_theme_path()]
 
 html_favicon = 'pancake-logo.png'
